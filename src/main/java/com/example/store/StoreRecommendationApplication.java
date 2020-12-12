a2ccbddb2c21dbbf13ae1c649099bfde4838ef8d
Lin Sirui <lsirui@redhat.com>
2020-12-12 10:34:18 +0800

    Update Jaeger usage

--- src/main/java/com/example/store/StoreRecommendationApplication.java
+++ src/main/java/com/example/store/StoreRecommendationApplication.java
@@ -1,8 +1,10 @@
 package com.example.store;
 
+import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -11,5 +13,10 @@ public class StoreRecommendationApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreRecommendationApplication.class, args);
     }
+
+//    @Bean
+//    public io.opentracing.Tracer tracer() {
+//        return Configuration.fromEnv().getTracer();
+//    }
 
 }
