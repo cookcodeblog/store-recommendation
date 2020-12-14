de58201020067fe3159c10f2eff0df80e40e9ca7
Lin Sirui <lsirui@redhat.com>
2020-12-14 20:18:04 +0800

    SpringBoot version

--- src/main/java/com/example/store/StoreRecommendationApplication.java
+++ src/main/java/com/example/store/StoreRecommendationApplication.java
@@ -1,22 +1,14 @@
 package com.example.store;
 
-import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
+
 @SpringBootApplication
 public class StoreRecommendationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreRecommendationApplication.class, args);
     }
-
-//    @Bean
-//    public io.opentracing.Tracer tracer() {
-//        return Configuration.fromEnv().getTracer();
-//    }
 
 }
--- src/main/java/com/example/store/controller/RecommendationController.java
+++ src/main/java/com/example/store/controller/RecommendationController.java
@@ -6,8 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 @Slf4j
 public class RecommendationController {
