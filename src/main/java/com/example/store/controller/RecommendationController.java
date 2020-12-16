0a11488ce01bf3ed6c5d7b9e006606fee70bacc5
Lin Sirui <lsirui@redhat.com>
2020-12-16 11:27:19 +0800

    Version 2

--- src/main/java/com/example/store/controller/RecommendationController.java
+++ src/main/java/com/example/store/controller/RecommendationController.java
@@ -12,7 +12,7 @@ import java.util.Random;
 @Slf4j
 public class RecommendationController {
 
-    private static final String RESPONSE_STRING_FORMAT = "recommendation v1 from '%s': %d\n";
+    private static final String RESPONSE_STRING_FORMAT = "recommendation v2 from '%s': %d\n";
 
     /**
      * Counter to help us see the lifecycle
